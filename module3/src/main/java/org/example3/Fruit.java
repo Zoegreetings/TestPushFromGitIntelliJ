bd657a91e251328cc6627712220f27876f899be2
pingyingzeng <zenggreetings@gamil.com>
2022-12-15 17:23:56 -0500

    test push from git

---
+++ module3/src/main/java/org/example3/Fruit.java
@@ -0,0 +1,18 @@
+package org.example3;
+
+public class Fruit {
+    private String name;
+    private String description;
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Fruit() {
+    }
+
+    public String isFruit() {
+        return "true";
+    }
+}
---
+++ src/main/java/org/example/Main2.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example3.Fruit;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Pear pear = new Pear();
+        System.out.println(pear.isHappy());
+        Fruit f = new Fruit();
+        System.out.printf("test push this is an fruit %s%n", f.isFruit());
+    }
+}
